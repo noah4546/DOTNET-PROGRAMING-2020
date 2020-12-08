78c6b0bf768ff36a93ee688141c1a59c449db318
Noah Tomkins <1tomkinsnoa@gmail.com>
2020-12-08 18:02:30 -0500

    Finished lab5a

--- Lab5A/Form1.Designer.cs
+++ Lab5A/Form1.Designer.cs
@@ -30,12 +30,89 @@ namespace Lab5A
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.colorBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.colorPicker = new System.Windows.Forms.ColorDialog();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // colorBtn
+            // 
+            this.colorBtn.Location = new System.Drawing.Point(12, 12);
+            this.colorBtn.Name = "colorBtn";
+            this.colorBtn.Size = new System.Drawing.Size(82, 28);
+            this.colorBtn.TabIndex = 0;
+            this.colorBtn.Text = "Color";
+            this.colorBtn.UseVisualStyleBackColor = true;
+            this.colorBtn.Click += new System.EventHandler(this.colorBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(100, 12);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(82, 28);
+            this.closeBtn.TabIndex = 1;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 125);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(90, 68);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(30, 70);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(334, 411);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.colorBtn);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "A Drop in the Buket";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button colorBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ColorDialog colorPicker;
+        private System.Windows.Forms.Timer timer;
     }
 }
 
--- Lab5A/Form1.cs
+++ Lab5A/Form1.cs
@@ -1,20 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/*
+ * Noah Tomkins
+ * September 21, 2020
+ * 
+ * A drop in the bukket.
+ * A bukket will fill with a liquid of the users choice, and
+ * will fill at a specified flowrate depending on a sliders value
+ * 
+ * I, Noah Tomkins, 000790079 certify that this material is my original work.  
+ * No other person's work has been used without due acknowledgement.
+ */
 
 namespace Lab5A
 {
     public partial class Form1 : Form
     {
+        private Graphics g;
+        private Pen p;
+        private SolidBrush b;
+        private SolidBrush bb;
+        private Color c = Color.Aqua;
+
+        private int waterMax = 275;
+        private int fillPercent = 0;
+
+        Rectangle water;
+        Rectangle flow;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// When the form loads initiate the paiting variables
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">PaintEventArgs</param>
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+            p = new Pen(Color.White);
+            b = new SolidBrush(c);
+            bb = new SolidBrush(Color.Black);
+
+            g = CreateGraphics();
+
+            Reset();
+        }
+
+        /// <summary>
+        /// When the color button is clicked show pop-up to
+        /// change the color of the liquid
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void colorBtn_Click(object sender, EventArgs e)
+        {
+            colorPicker.Color = c;
+            colorPicker.ShowDialog();
+            c = colorPicker.Color;
+        }
+
+        /// <summary>
+        /// When the timer ticks it will fill the bukket
+        /// with the liquid, the interval is changed by
+        /// the slider
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            fillPercent++;
+            water.Y = 100 + waterMax - fillPercent;
+            water.Height = fillPercent;
+            g.FillRectangle(b, water);
+            g.FillRectangle(b, flow);
+
+            if (fillPercent > 100)
+            {
+                fillPercent = 0;
+                trackBar1.Value = 0;
+                g.FillRectangle(bb, flow);
+                g.FillRectangle(b, water);
+                timer.Stop();
+            }
+                
+        }
+
+        /// <summary>
+        /// Resets the painted objects
+        /// </summary>
+        public void Reset()
+        {
+            g.Clear(Color.Black);
+
+            g.DrawLine(p, 74, 250, 74, 375);
+            g.DrawLine(p, 300, 250, 300, 375);
+            g.DrawLine(p, 74, 375, 300, 375);
+
+            flow = new Rectangle(108, 150, 20, 225);
+
+            water = new Rectangle(75, 100 + waterMax - fillPercent, 225, fillPercent);
+            g.FillRectangle(b, water);
+        }
+
+        /// <summary>
+        /// When the trackbar changes its value
+        /// update the interval. When it is 0 it will
+        /// stop the timer and stop the liquid, when it
+        /// is greater than 0 it will fill the bukket
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value == 0)
+            {
+                timer.Stop();
+                g.FillRectangle(bb, flow);
+                g.FillRectangle(b, water);
+            }
+            else if (trackBar1.Value >= 1)
+            {
+                Reset();
+                timer.Interval = (11 - trackBar1.Value) * 50;
+                timer.Start();
+            }
+        }
+
+        /// <summary>
+        /// When the close button is pressed it will close the application
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void closeBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
